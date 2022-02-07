f4033eca1fcbd65cb78bcd6be534eb5eefd7c4a3
austintheoz <98079457+austintheoz@users.noreply.github.com>
2022-02-06 21:42:21 -0800

    Update file_properties.py

--- WebStreamer/utils/file_properties.py
+++ WebStreamer/utils/file_properties.py
@@ -48,7 +48,7 @@ def get_media_from_message(message: "Message") -> Any:
 
 def get_hash(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
-    return getattr(media, "file_unique_id", "")[:6]
+    return getattr(media, "file_unique_id", "")[:100]
 
 def get_name(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
