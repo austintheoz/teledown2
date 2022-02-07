f140b86ca0b52c34128dfc63d0ccd7253185521d
austintheoz <98079457+austintheoz@users.noreply.github.com>
2022-02-07 08:40:24 -0800

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -6,8 +6,8 @@ from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
 
 
-@StreamBot.on_message(filters.command(["start", "help"]))
-async def start(_, m: Message):
-    await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
-    )
+# @StreamBot.on_message(filters.command(["start", "help"]))
+# async def start(_, m: Message):
+#     await m.reply(
+#         f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+#     )
